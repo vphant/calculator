6ffd4e9db3989528ff4e4a877b6611faa8b0b067
Vladyslav Zghodynskyi <vphant@gmail.com>
2022-08-08 16:59:57 +0300

    add basic calculator buttons functionality

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,18 @@
-$('.calc-button').on('click', function () {
-  alert('button clicked');
+// doc ready
+$(function () {
+  let mainInput = $('#main-input');
+  mainInput.val('');
+  mainInput.focus();
+});
+
+$('.input-button').on('click', function () {
+  let mainInput = $('#main-input');
+  var clickedBtn = $(this);
+  mainInput.val(mainInput.val() + clickedBtn.text());
+})
+
+$('#ac-button').on('click', function () {
+  let mainInput = $('#main-input');
+  mainInput.val('');
+  mainInput.focus();
 })
