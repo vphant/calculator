f4dbc8a0b42788c12958a145b1651ea5b5432035
Vladyslav Zghodynskyi <vphant@gmail.com>
2022-08-08 17:30:46 +0300

    add focus

--- public/js/app.js
+++ public/js/app.js
@@ -9,6 +9,7 @@ $('.input-button').on('click', function () {
   let mainInput = $('#main-input');
   var clickedBtn = $(this);
   mainInput.val(mainInput.val() + clickedBtn.attr('data-val'));
+  $('#main-input').focus();
 })
 
 $('#ac-button').on('click', function () {
