338c5a295fa4195c09c1823b828eb20d94f5559e
Vladyslav Zghodynskyi <vphant@gmail.com>
2022-08-08 14:58:06 +0300

    add twig and homepage index action

---
+++ src/Controller/HomepageController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomepageController extends AbstractController
+{
+    /**
+     * @Route("/")
+     */
+    public function index(): Response
+    {
+        $number = random_int(0, 100);
+
+        return $this->render('homepage/index.html.twig', [
+            'number' => $number,
+        ]);
+    }
+}
