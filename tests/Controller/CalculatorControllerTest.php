924225a9b0f9bbb8d2b807600c76a70a451dcac9
Vladyslav Zghodynskyi <vphant@gmail.com>
2022-08-09 23:43:08 +0300

    add data provider

--- tests/Controller/CalculatorControllerTest.php
+++ tests/Controller/CalculatorControllerTest.php
@@ -6,13 +6,34 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class CalculatorControllerTest extends WebTestCase
 {
-    public function testCalculate(): void
+    /**
+     * @dataProvider getSuccessCases
+     */
+    public function testCalculate($inputValue, $expectedResult): void
     {
         $client = static::createClient();
-        $client->jsonRequest('POST', '/calculate', ['inputValue' => '55']);
+        $client->jsonRequest('POST', '/calculate', ['inputValue' => $inputValue]);
         $this->assertResponseIsSuccessful();
 
         $responseData = json_decode($client->getResponse()->getContent(), true);
-        $this->assertEquals(55, $responseData['result']);
+        $this->assertEquals($expectedResult, $responseData['result']);
+    }
+
+    public function getSuccessCases()
+    {
+        return [
+            [
+                '55',
+                55
+            ],
+            [
+                '',
+                0
+            ],
+            [
+                '-55.19 * 2 + 66.11 / -2 - 77 + - 55.777 * 1 * 3 * -0.095 - 50',
+                -254.538555
+            ]
+        ];
     }
 }
