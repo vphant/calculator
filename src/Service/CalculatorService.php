960dbfe7be145e8f9c462602cc3a41050e988af2
Vladyslav Zghodynskyi <Vladyslav.Zghodynskyi@noveogroup.com>
2022-08-09 19:47:25 +0300

    do calculation

--- src/Service/CalculatorService.php
+++ src/Service/CalculatorService.php
@@ -11,12 +11,71 @@ class CalculatorService
         }
 
         // group 1 - operand (+-*/)
-        // group 2 - number (with or without - and with or about .float part)
-        $result = preg_match_all('/(^|[-+*\/])(-?\d+(\.\d+)?)/', $inputValue, $mathes);
+        // group 2 - number (with or without "-" and with or about .float part)
+        $result = preg_match_all('/(^|[-+*\/])(-?\d+(\.\d+)?)/', $inputValue, $matches);
 
-        $operands = $mathes[1];
-        $numbers = $mathes[2];
+        $operands = $matches[1];
+        $numbers = array_map('floatval', $matches[2]);
 
-        return 9999999;
+        // filter first empty pseudo operand
+        $operands = array_filter($operands, function ($item) {
+            return !empty($item);
+        });
+
+        // reset indexes
+        $operands = array_values($operands);
+
+        while (count($operands) > 0) {
+            $firstOperand = $this->searchFirstInArray(['*', '/'], $operands);
+
+            if (false === $firstOperand) {
+                $firstOperand = $this->searchFirstInArray(['+', '-'], $operands);
+                if (false === $firstOperand) {
+                    break;
+                }
+            }
+
+            $firstOperandKey = $firstOperand['key'];
+            $firstOperandVariant = $firstOperand['needle'];
+
+            $operationResult = match ($firstOperandVariant) {
+                '*' => $numbers[$firstOperandKey] * $numbers[$firstOperandKey + 1],
+                '/' => $numbers[$firstOperandKey] / $numbers[$firstOperandKey + 1],
+                '+' => $numbers[$firstOperandKey] + $numbers[$firstOperandKey + 1],
+                '-' => $numbers[$firstOperandKey] - $numbers[$firstOperandKey + 1],
+            };
+
+            $numbers[$firstOperandKey] = $operationResult;
+            unset($numbers[$firstOperandKey + 1]);
+            unset($operands[$firstOperandKey]);
+
+            $operands = array_values($operands);
+            $numbers = array_values($numbers);
+        }
+
+        if (count($numbers) > 1) {
+            throw new \Exception('Numbers exceed operands.');
+        }
+
+        return array_shift($numbers);
     }
+
+    private function searchFirstInArray(array $needles, array $haystack)
+    {
+        $filtered = array_filter($haystack, function ($item) use ($needles) {
+            return in_array($item, $needles);
+        });
+
+        if (empty($filtered)) {
+            return false;
+        }
+
+        $firstKey = array_key_first($filtered);
+
+        return [
+            'key' => $firstKey,
+            'needle' => $filtered[$firstKey]
+        ];
+    }
+
 }
