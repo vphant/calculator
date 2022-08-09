b5d78006ccd32513e024d49a6c6543d03e60b49a
Vladyslav Zghodynskyi <Vladyslav.Zghodynskyi@noveogroup.com>
2022-08-09 18:11:18 +0300

    parse input value string

--- src/Service/CalculatorService.php
+++ src/Service/CalculatorService.php
@@ -10,7 +10,12 @@ class CalculatorService
             return 0;
         }
 
-        # TODO: calculate
+        // group 1 - operand (+-*/)
+        // group 2 - number (with or without - and with or about .float part)
+        $result = preg_match_all('/(^|[-+*\/])(-?\d+(\.\d+)?)/', $inputValue, $mathes);
+
+        $operands = $mathes[1];
+        $numbers = $mathes[2];
 
         return 9999999;
     }
