730d22357444cde08ffadac9bb89a6299cd257b0
Vladyslav Zghodynskyi <vphant@gmail.com>
2022-08-11 12:31:42 +0300

    remove unused use

--- src/Controller/CalculatorController.php
+++ src/Controller/CalculatorController.php
@@ -8,7 +8,6 @@ use App\Service\CalculatorService;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
